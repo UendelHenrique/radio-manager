0569e44d549a7f04db460c20aa7983551a2068e8
Uendel Henrique Falcao Nogueira <uendeln@pjm.com.br>
2026-04-27 12:40:02 -0400

    Allow external access by binding to 0.0.0.0

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
